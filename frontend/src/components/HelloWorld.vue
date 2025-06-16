0804c586b63b68272b6d517ea2d5dc175aa4f3b7
pmunne <munnepau@gmail.com>
2025-06-16 11:36:47 +0200

    vue-setup and scss setup

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -34,8 +34,8 @@ const count = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .read-the-docs {
-  color: #888;
+  color: $primary-color;
 }
 </style>
