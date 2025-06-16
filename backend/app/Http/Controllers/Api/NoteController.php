789b9348311c9feecba78457d08813986682ec7c
pmunne <munnepau@gmail.com>
2025-06-16 23:16:39 +0200

    Paginate, search  and comments on Note Crud

--- backend/app/Http/Controllers/Api/NoteController.php
+++ backend/app/Http/Controllers/Api/NoteController.php
@@ -9,16 +9,37 @@ use App\Models\Note;
 class NoteController extends Controller
 {
 
-    public function index() 
+    /**
+     *  Returns a list of notes (6 per page).
+     *  If finds a search input param, it filters by title.
+     */
+    public function index(Request $request) 
     {
-        return Note::all();
+        $finder = $request->input('search');
+        $query = Note::query();
+
+        if(!empty($finder)) {
+           $query->where('title', 'like', "%{$finder}%");
+
+        }
+        return response()->json(
+            $query->latest()->paginate(6)
+        );
+
     }
 
+    /**
+     * Returns one specific note.
+     */
     public function show(Note $note) 
     {
         return response()->json($note, 200);
     }
 
+    /**
+     * Stores a new Note.
+     * Requires: title = "string(255)" and content=text.
+     */
     public function store(Request $request) 
     {
         $data = $this->validateNote($request);
@@ -27,6 +48,9 @@ class NoteController extends Controller
         return response()->json($note, 201);
     }
 
+    /**
+     * Update an existing note
+     */
     public function update(Request $request, Note $note) 
     {
         $data = $this->validateNote($request, updating:true);
@@ -35,6 +59,9 @@ class NoteController extends Controller
         return response()->json($note, 200);
     }
 
+    /**
+     * Deletes an existing note.
+     */
     public function destroy(Note $note) 
     {
         $note->delete(); 
@@ -42,6 +69,10 @@ class NoteController extends Controller
 
     }
 
+    /**
+     * Validate the input data. 
+     * If updating is true, allows partial updates. 
+     */
     private function validateNote(Request $request, bool $updating = false) 
     {
         return $request->validate([
@@ -51,6 +82,9 @@ class NoteController extends Controller
 
     }
 
+    /**
+     * Create a note from the validated data.
+     */
     private function createNote(array $data)  
     {
         return Note::create($data);
