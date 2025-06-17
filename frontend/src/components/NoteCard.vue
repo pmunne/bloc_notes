a0afb95d5c091c89f535d86f3f0efb7d30ac9835
pmunne <munnepau@gmail.com>
2025-06-17 23:54:06 +0200

    NoteList simple view done

---
+++ frontend/src/components/NoteCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="note-card">
+        <h3>{{ note.title }}</h3>
+        <p>{{ note.content }}</p>
+        <small>{{ formatDate(note.created_at) }}</small>
+    </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+    note: {
+        id: number;
+        title: string;
+        content: string;
+        created_at: string;
+
+    }
+}>();
+
+const formatDate = (dateStr: string) => {
+    return new Date(dateStr).toLocaleDateString();
+}
+
+</script>
+<style scoped lang="scss">
+small {
+    color: $primary_color;
+}
+
+</style>
--- frontend/src/views/NotesList.vue
+++ frontend/src/views/NotesList.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <h2>Notes</h2>
-            <p>hOLA</p>
+        <div v-if="notes.length === 0">
+            <p>Ther are no notes yet.</p>
+        </div>
+        <div v-else class="note-list">
+            <NoteCard v-for="note in notes" :key="note.id" :note="note"/>
+        </div>
+        <div class="pagination">
+            <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+            <span>{{ currentPage }}</span>
+            <button @click="nextPage" :disabled="currentPage === lastPage">Next</button>
+        </div>    
     </div>
 
 </template>
@@ -9,6 +19,8 @@
 
 import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
+import NoteCard from '@/components/NoteCard.vue';
+
 const notes = ref([]);
 const currentPage = ref(1);
 const lastPage = ref(1);
@@ -16,17 +28,18 @@ const lastPage = ref(1);
 const fetchNotes = async () => {
     try {
         const res = await axios
-            .get(`http://localhost/api/notes?page=${currentPage.value}`, {
+            .get(`http://localhost:8000/api/notes?page=${currentPage.value}`, {
                 headers: {
                     Authentication: 'Doonamis',
                 },
             });
-            console.log(notes.value);
+       
             notes.value = res.data.data;
+            console.log(notes.value);
             lastPage.value = res.data.last_page;
 
         } catch (err) {
-            console.error('Error trying to recive notes', err);
+            console.error('Error trying to receive notes', err);
         }
     };
 onMounted(fetchNotes);
