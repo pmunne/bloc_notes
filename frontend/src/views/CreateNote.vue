230fefb6f03c2af8c068b140be8fc65c0dd5aeb6
pmunne <munnepau@gmail.com>
2025-06-18 09:39:55 +0200

    Create note simple done

--- frontend/src/views/CreateNote.vue
+++ frontend/src/views/CreateNote.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="create-note">
-        <p>Hello</p>
+        <form @submit.prevent="addNote">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" id="title" v-model="note.title" required>
+            </div>
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea name="content" id="content" v-model="note.content" rows="6" required></textarea>
+            </div>
+
+            <button type="submit">Save note</button>
+        </form>
     </div>
 </template>
 
