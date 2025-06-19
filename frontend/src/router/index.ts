da995ea3c3664a2d0a1840b64889a1accac0c506
pmunne <munnepau@gmail.com>
2025-06-19 10:53:17 +0200

    route/index cleanup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import NotesList from '@/views/NotesList.vue';
-import CreateNote from '@/views/CreateNote.vue';
-import EditNote from '@/views/EditNote.vue';
 
 const routes = [
     { 
         path: '/',
         name: 'Home', 
         component: NotesList
-    },
-    {
-        path: '/create',
-        name: 'CreateNote',
-        component: CreateNote
-    },
-    {
-        path: '/edit/:id',
-        name: 'EditNote',
-        component: EditNote
     }
 ];
 
