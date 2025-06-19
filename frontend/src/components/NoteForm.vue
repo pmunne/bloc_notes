88eae90aa13ff328b7c3194a855c9ca499c2dede
pmunne <munnepau@gmail.com>
2025-06-19 10:52:33 +0200

    Style documentation done

--- frontend/src/components/NoteForm.vue
+++ frontend/src/components/NoteForm.vue
@@ -17,7 +17,7 @@
                 {{ isSaving ? (isUpdating ? 'Updating...' : 'Saving...') :(isUpdating ? 'Update note' : 'Save note') }}
                 <span v-if="isSaving" class="loading-spinner inline"/>
             </button>
-            <button v-if="isUpdating" type="button" class="btn btn-secondary" @click="$emit('cancel')" >Cancel edit</button>
+            <button v-if="isUpdating" type="button" class="btn" @click="$emit('cancel')" >Cancel edit</button>
         </form>
 
     </div>
